a359f7798e0149056775a3c96b4c9f8e2ca135d0
Tamas Czinege <tomi.czinege@gmail.com>
2012-07-27 12:33:45 +0100

    added missing typedefs & .gitignore

---
+++ src/typedef.h
@@ -0,0 +1,18 @@
+#ifndef libpx_typedef_h
+#define libpx_typedef_h
+
+typedef struct px_connection px_connection;
+typedef struct px_connection_params px_connection_params;
+typedef struct px_data_cell px_data_cell;
+typedef struct px_data_row px_data_row;
+typedef struct px_error px_error;
+typedef struct px_message px_message;
+typedef struct px_parameter px_parameter;
+typedef struct px_query px_query;
+typedef struct px_response px_response;
+typedef struct px_response_list px_response_list;
+typedef struct px_result px_result;
+typedef struct px_result_list px_result_list;
+typedef struct px_row_description_column px_row_description_column;
+
+#endif /* libpx_typedef_h */
